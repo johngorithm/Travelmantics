8ff267733b181a7b87397dd27f74507867fa10de
John Obi <johngorithm@gmail.com>
2019-08-04 03:05:03 +0100

    Connect to InsertActivity from ListActivity

--- app/src/main/java/com/jwx/travelmantics/deal_listing/ListActivity.java
+++ app/src/main/java/com/jwx/travelmantics/deal_listing/ListActivity.java
@@ -1,12 +1,18 @@
 package com.jwx.travelmantics.deal_listing;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.jwx.travelmantics.R;
 import com.jwx.travelmantics.common.BaseActivity;
+import com.jwx.travelmantics.deal_creation.InsertActivity;
 import com.jwx.travelmantics.models.TravelDeal;
 
 public class ListActivity extends BaseActivity implements DealListView {
@@ -32,7 +38,27 @@ public class ListActivity extends BaseActivity implements DealListView {
         dealRecyclerView.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_activity_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_deal_option:
+                showInsertDealActivity();
+                break;
+        }
+        return true;
+    }
+
+    private void showInsertDealActivity() {
+        Intent intent = new Intent(ListActivity.this, InsertActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onStart() {
